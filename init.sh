f4c840e039fbf9edbc95842a9d1c7a3ea9bb54c0
Brian <silva.brian.987@gmail.com>
2020-07-15 00:39:58 -0300

    Agregue logica para ejecucion programada con crond

--- init.sh
+++ init.sh
@@ -1,9 +1,19 @@
 APP_ARGS=""
 
-if [-n "$LOG_LEVEL"]; then
+if [ -n "$LOG_LEVEL" ]
+then
     APP_ARGS="$APP_ARGS -v $LOG_LEVEL"
 fi
 
 APP_ARGS="$APP_ARGS -u $FREENOM_USERNAME -p $FREENOM_PASSWORD -d $FREENOM_DOMAINS"
 
-python ./app.py $APP_ARGS
+if [ -n "$SCHEDULED" ]
+then
+    python ./app.py $APP_ARGS
+    echo "#!/bin/sh" > /etc/periodic/15min/freenom_domain_updater_job
+    echo "python ./app.py $APP_ARGS" >> /etc/periodic/15min/freenom_domain_updater_job
+    chmod a+x /etc/periodic/15min/freenom_domain_updater_job
+    crond -l 2 -f
+else
+    python ./app.py $APP_ARGS
+fi
