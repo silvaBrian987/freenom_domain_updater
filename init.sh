19c501cdeccb2e69706299346d7379b4cab5f24b
Brian <silva.brian.987@gmail.com>
2020-07-15 22:45:17 -0300

    Validacion de pre ejecucion python en ejecucion programada

--- init.sh
+++ init.sh
@@ -1,17 +1,20 @@
+#!/bin/sh
+
 APP_ARGS=""
 
-if [ -n "$LOG_LEVEL" ]
-then
+if [ -n "$LOG_LEVEL" ]; then
     APP_ARGS="$APP_ARGS -v $LOG_LEVEL"
 fi
 
 APP_ARGS="$APP_ARGS -u $FREENOM_USERNAME -p $FREENOM_PASSWORD -d $FREENOM_DOMAINS"
 
-if [ -n "$SCHEDULED" ]
-then
+if [ -n "$SCHEDULED" ]; then
     python ./app.py $APP_ARGS
-    echo "#!/bin/sh" > /etc/periodic/15min/freenom_domain_updater_job
-    echo "python ./app.py $APP_ARGS" >> /etc/periodic/15min/freenom_domain_updater_job
+    if [ $? -ne 0 ]; then
+        exit $?
+    fi
+    echo "#!/bin/sh" >/etc/periodic/15min/freenom_domain_updater_job
+    echo "python ./app.py $APP_ARGS" >>/etc/periodic/15min/freenom_domain_updater_job
     chmod a+x /etc/periodic/15min/freenom_domain_updater_job
     crond -l 2 -f
 else
